b1a2c8fd74935502bcfe8113d5483d09775db794
cecilia <lcecilia8147@gmail.com>
2023-02-11 18:04:29 -0300

    se agrega css e img

--- pizzeria-lorenzini/src/App.vue
+++ pizzeria-lorenzini/src/App.vue
@@ -4,7 +4,40 @@ console.log('hola vue')
 </script>
 
 <template>
-  <router-link to="/register">register</router-link>
-    <router-link to="/">home</router-link>
+    <div id="navbar">
+    <nav class="navbar navbar-expand-lg bg-danger">
+      <div class="container">
+        <a class="navbar-brand" href="Index.html">Pizzeria Lorenzini</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" ><router-link to="/">home</router-link></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Carta</a>
+            </li>
+          </ul>
+          <div class="container">
+            <div class="row">
+              <div class="col-3 btn-group offset-8" role="group">
+                <button type="button" class="btn btn-outline-dark">
+                  <a class="nav-link" href="/login.html">Log in</a>
+                </button>
+                <button type="button" class="btn btn-dark">
+                    <router-link to="/register">register</router-link>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+    
  <RouterView></RouterView>
 </template>
--- pizzeria-lorenzini/src/components/Home.vue
+++ pizzeria-lorenzini/src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Soy pagina principal HOME</h1>
+   <div class="divPizzeria">
+    <img src="/img/pizza.jpg" alt="pizzeria lorenzini imagen principal" class="imgPizzeria" />
+  </div>
+  <div class="container second-part">
+    <div class="input-group">
+      <input type="text" class="form-control">
+      
+      <span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+          class="bi bi-search" viewBox="0 0 16 16">
+          <path
+            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg></span>
+    </div>
+  </div>
+
 </template>
 
 <script setup>
