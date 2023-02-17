3cf174128dd4535fbf4057bd49965a821a6805cf
cecilia <lcecilia8147@gmail.com>
2023-02-17 09:21:39 -0300

    se agrega obj

--- pizzeria-lorenzini/src/components/Home.vue
+++ pizzeria-lorenzini/src/components/Home.vue
@@ -132,17 +132,17 @@ console.log(pizza)
             <!--modal boton +-->
             <div  class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog" >
-                <div class="modal-content"  >
+                <div class="modal-content" v-for="pi in pizza" >
                   <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">{{ pizza.Nombre }}</h1>
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">{{ pi.Nombre }}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
                     <div >
-                      <img :src=pizza.img />
+                      <img :src=pi.img />
                     </div>
                     <div>
-                      {{  pizza.Ingredientes }}
+                      {{  pi.Ingredientes }}
                     </div>
                     <div class="mb-3">
                       <p>Unidades</p><button id="disminuir" @click="disminuir()" class="btn btn-danger">-</button><input class="cantidad" type="text" value ="0" id="cantidad"/><button class="btn btn-danger" id="aumentar" @click="aumentar()">+</button>
